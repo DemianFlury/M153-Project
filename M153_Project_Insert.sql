3a79d6d24d26f103877609f619e39cb85f4883a9
Demian Flury <74318556+DemianFlury@users.noreply.github.com>
2022-12-01 10:38:29 +0100

    add checkident and delete

--- M153_Project_Insert.sql
+++ M153_Project_Insert.sql
@@ -1,3 +1,10 @@
+delete from Customer, Creator, Course, Visit;
+
+dbcc checkident(Customer, reseed, 0);
+dbcc checkident(Creator, reseed, 0);
+dbcc checkident(Course, reseed, 0);
+dbcc checkident(Visit, reseed, 0);
+
 INSERT INTO Customer(Age, Passwd, Email, FirstName, LastName) VALUES 
 (23, 'ABC', 'franz@gmail.com', 'Franz', 'Herter'),
 (34, 'JucktKeine', 'gilbert@gmx.ch', 'Gilbert', 'Gottfried'),
@@ -54,15 +61,15 @@ INSERT INTO Course(Price, CourseDescription, CourseName, CreationDate, fk_Creato
 (150.50,'','Calisthenics',02-02-2020, 7),
 (60,'','Yoga-Lektion',02-02-2020, 8),
 (265,'','Trainingsplan Oberkörper',02-02-2020, 9),
-(369.95,'','',02-02-2020, 10),
+(369.95,'','Posing Basics',02-02-2020, 10),
 (420,'','Trainingsplan Oberkörper',02-02-2020, 11),
 (240,'','Trainingsplan Beine',02-02-2020, 12),
 (80,'','Trainingsplan Oberkörper',02-02-2020, 13),
 (123,'','Gym Habits',02-02-2020, 14),
 (450,'','Trainingsplan Beine',02-02-2020, 2),
-(250,'','',02-02-2020, 15),
-(550,'','',02-02-2020, 16),
+(250,'','Macher Mindset',02-02-2020, 15),
+(550,'','Calisthenics',02-02-2020, 16),
 (700,'','Trainingsplan Beine',02-02-2020, 17),
 (400,'','Gym Habits',02-02-2020, 18),
 (420,'','Trainingsplan Oberkörper',02-02-2020, 19),
-(365,'','',02-02-2020, 20);
+(365,'','Trainingsplan Beine',02-02-2020, 20);
