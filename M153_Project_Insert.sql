fae00470efd2ce7e3f887a349ae1effe302aacaf
Demian Flury <74318556+DemianFlury@users.noreply.github.com>
2022-12-15 10:52:04 +0100

    update insert

--- M153_Project_Insert.sql
+++ M153_Project_Insert.sql
@@ -1,10 +1,8 @@
-EXEC sp_MSForEachTable "ALTER TABLE ? NOCHECK CONSTRAINT all"
+use FitnessCourse;
+go
 
-delete from Customer;
-delete from Creator;
-delete from Course;
-delete from Visit;
-
+exec sp_MSForEachTable "ALTER TABLE ? NOCHECK CONSTRAINT all"
+exec sp_MSForEachTable "DELETE FROM ?"
 exec sp_MSForEachTable "ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all"
 
 dbcc checkident(Customer, reseed, 0);
@@ -12,6 +10,7 @@ dbcc checkident(Creator, reseed, 0);
 dbcc checkident(Course, reseed, 0);
 dbcc checkident(Visit, reseed, 0);
 
+go
 
 INSERT INTO Customer(Age, Passwd, Email, FirstName, LastName) VALUES 
 (23, 'ABC', 'franz@gmail.com', 'Franz', 'Herter'),
